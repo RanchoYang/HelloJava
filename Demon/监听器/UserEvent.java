76427ffaaf605f54f59eaf4a5d06f931442974e9
Rancho <49603180+RanchoYang@users.noreply.github.com>
2020-05-21 18:51:07 +0800

    Add files via upload

---
+++ Demon/监听器/UserEvent.java
@@ -0,0 +1,15 @@
+package com.listener;
+
+public class UserEvent {
+
+	private String name;
+
+	public UserEvent(String name) {
+		this.name = name;
+	}
+
+	public String getUser() {
+		return this.name;
+	}
+
+}
---
+++ Demon/监听器/UserListener.java
@@ -0,0 +1,8 @@
+package com.listener;
+
+public interface UserListener {
+
+	void work(UserEvent userEvent);
+
+	void talk(UserEvent userEvent);
+}
